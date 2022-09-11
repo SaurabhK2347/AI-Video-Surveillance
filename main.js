0e26c2a86ca362153531c7d793aa88705a15ad2a
SaurabhK2347 <81204284+SaurabhK2347@users.noreply.github.com>
2022-09-11 11:33:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects = [];
+status = "";
 video ="";
 
 function preload() 
@@ -15,6 +17,33 @@ function setup()
 function draw() 
 {
     image(video, 0, 0, 480, 380);
+    if(status != "") 
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) 
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("black");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("black");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
+
+function gotResult(error, results) 
+{
+    if (error) 
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() 
